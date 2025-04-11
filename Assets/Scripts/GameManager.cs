3823e02c354dc89b052b00f581a353980008c180
YunLin Huang <karta035865@gmail.com>
2025-04-11 23:28:24 +0900

    Add fonts and basic game features

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,50 @@
 ﻿using System.Data;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    #region SerializeField
+    [SerializeField] GameObject gamePanel;
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] TextMeshProUGUI spell;
+    [SerializeField] TextMeshProUGUI translate;
+    [SerializeField] TextMeshProUGUI example;
+    [SerializeField] TMP_InputField input;
+    #endregion
     SimpleDB db = new();
+    bool readyToNext = false;
+    bool waitingForRelease = false;
+    int next = 0;
+    DataTable table;
     void Start()
     {
         int from = 1;
         int to = 43;
-        //db.CreateDB();
-        var table = ReadWordsInRange(from, to, 5);
-        SimpleDB.PrintDataTable(table);
+        table = ReadWordsInRange(from, to, 43);
+        ShowAnswer(false);
+        LoadNextWord();
+    }
 
+    void Update()
+    {
+        DetectEnter();
+    }
+
+    public void OnTextSubmit(string text)
+    {
+        if (input.text == spell.text)
+        {
+            ShowAnswer(true);
+            readyToNext = true;
+            waitingForRelease = true;
+        }
+        input.text = "";
+        input.ActivateInputField();
     }
     DataTable ReadWordsInRange(int from, int to, int quantity)
     {
-        // TODO: Write SQL query command
         string command = $@"
             SELECT * 
             FROM Vocabulary
@@ -25,5 +54,49 @@ public class GameManager : MonoBehaviour
 
         DataTable wordsInRange = db.GetTableFromSQLcommand(command);
         return wordsInRange;
+    }
+
+    void LoadNextWord()
+    {
+        DataRow row = table.Rows[next];
+        questionText.text = row["単語"].ToString();
+        spell.text = row["綴り"].ToString();
+        translate.text = row["中国語"].ToString();
+        example.text = RemoveParentheses(row["例"].ToString());
+        next += 1;
+    }
+    void ShowAnswer(bool show)
+    {
+        spell.enabled = show;
+        translate.enabled = show;
+        example.enabled = show;
+    }
+
+    void DetectEnter()
+    {
+        // 如果還在等你鬆開按鍵，就不處理下一題的行為
+        if (waitingForRelease)
+        {
+            // 檢查是否已鬆開 Enter
+            if (Input.GetKeyUp(KeyCode.Return))
+            {
+                // 一旦鬆開後，才允許偵測下一次按 Enter
+                waitingForRelease = false;
+            }
+            return;  // 跳出 Update，不做後面
+        }
+
+        // 真正偵測第二次按 Enter
+        if (readyToNext && Input.GetKeyDown(KeyCode.Return))
+        {
+            ShowAnswer(false);
+            LoadNextWord();
+            readyToNext = false;
+        }
+    }
+
+    string RemoveParentheses(string text)
+    {
+        return Regex.Replace(text, "[()]", "");
     }
 }
