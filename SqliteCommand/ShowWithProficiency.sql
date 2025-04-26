bf6b8b4f7960f7bad0e94ca84628dae4ececa65a
JPHOMEDESK\karta <karta035865@gmail.com>
2025-04-26 17:10:01 +0900

    Update database

--- SqliteCommand/ShowWithProficiency.sql
+++ SqliteCommand/ShowWithProficiency.sql
@@ -3,8 +3,6 @@ WITH subset AS (
       FROM Vocabulary
      WHERE タイプ = '通常'         -- ① 類別篩選
      ORDER BY 番号                 -- ② 先照編號排序
-     LIMIT 200               -- ③ 取 a..b 範圍
-     OFFSET 0
 )
 SELECT  S.*,                              
         COALESCE(U.Proficiency, 0) AS Proficiency,
@@ -12,4 +10,4 @@ SELECT  S.*,
 FROM    subset  AS S
 LEFT JOIN UserProgress AS U
        ON U.番号 = S.番号                  
-ORDER BY LastAnswer
+ORDER BY Proficiency
